3547622145df86943f6da386f7b2ea4715c64bfb
Tom Phily <tom.phily@live.fr>
2016-12-23 20:59:50 +0100

    #6 Ajout de la methode JoinChord

--- client/src/main/java/com/m1miageprojet/chord/ChordPeer.java
+++ client/src/main/java/com/m1miageprojet/chord/ChordPeer.java
@@ -1,5 +1,7 @@
 package com.m1miageprojet.chord;
 
+import java.util.Random;
+
 /**
  * 
  * @author Tom
@@ -10,6 +12,14 @@ public class ChordPeer
 	private int myId;
 	private ChordPeer succ;
 	private ChordPeer pred;
+	
+	public ChordPeer()
+	{
+		Random rand = new Random();
+		setMyId(rand.nextInt(101));
+		setSucc(this);
+		setPred(this);
+	}
 	
 	/**
 	 * @return the myId
@@ -70,6 +80,10 @@ public class ChordPeer
 	 */
 	public ChordPeer findkey(int key)
 	{
+		if(getPred() == this)
+		{
+			return this;
+		}
 		if(getPred().getMyId() < this.getMyId() && key > getPred().getMyId() && key <= this.getMyId())
 		{
 			return this;
@@ -82,5 +96,19 @@ public class ChordPeer
 		{
 			return getSucc().findkey(key);
 		}
+	}
+	
+	/**
+	 * join the node in chord 
+	 * @param ChordPeerhandle
+	 */
+	public void JoinChord(ChordPeer ChordPeerhandle)
+	{
+		ChordPeer S = ChordPeerhandle.findkey(getMyId());
+		ChordPeer pred = S.getPred();
+		S.setPred(this);
+		this.setPred(pred);
+		pred.setSucc(this);
+		this.setSucc(S);
 	}
 }
--- client/src/test/java/com/m1miageprojet/chord/ChordPeerTest.java
+++ client/src/test/java/com/m1miageprojet/chord/ChordPeerTest.java
@@ -1,6 +1,7 @@
 package com.m1miageprojet.chord;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import junit.framework.TestCase;
 
@@ -12,9 +13,9 @@ import junit.framework.TestCase;
 public class ChordPeerTest extends TestCase 
 {
 	/**
-	 * test si findkey renvoi le bon noeud avec une cle inferieur a l'id du noeud apellant
+	 * test la premiere condition de findKey
 	 */
-	public void testChordAvecCleInferieurAID()
+	public void testPremiereConditionFindKey()
 	{
 		ArrayList<ChordPeer> chord = new ArrayList<ChordPeer>();
 		for(int i =0; i<4; i++)
@@ -40,9 +41,37 @@ public class ChordPeerTest extends TestCase
 	}
 	
 	/**
-	 * test si findkey renvoi le bon noeud avec une cle suppérieur a l'id du noeud apellant
+	 * test la deuxieme condition de findKey
 	 */
-	public void testChordAvecCleSuperieurAID()
+	public void testDeuxiemeConditionDeFindKey()
+	{
+		ArrayList<ChordPeer> chord = new ArrayList<ChordPeer>();
+		for(int i =0; i<4; i++)
+		{
+			chord.add(new ChordPeer());
+		}
+		chord.get(0).setMyId(8);
+		chord.get(1).setMyId(13);
+		chord.get(2).setMyId(25);
+		chord.get(3).setMyId(96);
+		
+		chord.get(0).setSucc(chord.get(1));
+		chord.get(1).setSucc(chord.get(2));
+		chord.get(2).setSucc(chord.get(3));
+		chord.get(3).setSucc(chord.get(0));
+		
+		chord.get(0).setPred(chord.get(3));
+		chord.get(1).setPred(chord.get(0));
+		chord.get(2).setPred(chord.get(1));
+		chord.get(3).setPred(chord.get(2));
+		
+		assertEquals(chord.get(0), chord.get(3).findkey(2));
+	}
+	
+	/**
+	 * test la troixieme condition de findKey
+	 */
+	public void testTroixiemeConditionDeFindKey()
 	{
 		ArrayList<ChordPeer> chord = new ArrayList<ChordPeer>();
 		for(int i =0; i<4; i++)
@@ -68,31 +97,25 @@ public class ChordPeerTest extends TestCase
 	}
 	
 	/**
-	 * test si findkey renvoi le bon noeud avec une cle inferieur a l'id du noeud apellant
-	 * avec le dernier noeud
+	 * test si les anneaux se rajoute bien dans le reseau
 	 */
-	public void testChordAvecDernierNoeud()
+	public void testJoinChord()
 	{
-		ArrayList<ChordPeer> chord = new ArrayList<ChordPeer>();
-		for(int i =0; i<4; i++)
+		ChordPeer c1 = new ChordPeer();
+		ChordPeer c2 = new ChordPeer();
+		Random rand = new Random();
+		while(c1.getMyId() == c2.getMyId())
 		{
-			chord.add(new ChordPeer());
+			c2.setMyId(rand.nextInt(101));
 		}
-		chord.get(0).setMyId(8);
-		chord.get(1).setMyId(13);
-		chord.get(2).setMyId(25);
-		chord.get(3).setMyId(96);
-		
-		chord.get(0).setSucc(chord.get(1));
-		chord.get(1).setSucc(chord.get(2));
-		chord.get(2).setSucc(chord.get(3));
-		chord.get(3).setSucc(chord.get(0));
-		
-		chord.get(0).setPred(chord.get(3));
-		chord.get(1).setPred(chord.get(0));
-		chord.get(2).setPred(chord.get(1));
-		chord.get(3).setPred(chord.get(2));
-		
-		assertEquals(chord.get(0), chord.get(3).findkey(2));
+		System.out.println(c1.getMyId());
+		System.out.println(c2.getMyId());
+		c2.JoinChord(c1);
+
+		assertEquals(c1, c2.getPred());
+		assertEquals(c1, c2.getSucc());
+
+		assertEquals(c2, c1.getPred());
+		assertEquals(c2, c1.getSucc());
 	}
 }
