68d2fd202b4957f8bc3ad54dbdc222d36714a2ff
Estelle Rostan <estelle.rostan@etu.unice.fr>
2016-12-31 10:55:57 +0100

    Refactor des tests
    Utilisent setUp() et tearDown() de JUnit 3 ce qui permet de supprimer
    les duplications de code.

--- client/src/test/java/com/m1miageprojet/chord/ChordPeerTest.java
+++ client/src/test/java/com/m1miageprojet/chord/ChordPeerTest.java
@@ -10,123 +10,87 @@ import junit.framework.TestCase;
  * @author Tom
  *
  */
-public class ChordPeerTest extends TestCase 
-{
+public class ChordPeerTest extends TestCase {
+
+	private ArrayList<ChordPeer> chord;
+	private ChordPeer c1;
+	private ChordPeer c2;
+
+	public void setUp() {
+		chord = new ArrayList<ChordPeer>();
+		for (int i = 0; i < 4; i++) {
+			chord.add(new ChordPeer());
+		}
+		chord.get(0).setMyId(8);
+		chord.get(1).setMyId(13);
+		chord.get(2).setMyId(25);
+		chord.get(3).setMyId(96);
+
+		chord.get(0).setSucc(chord.get(1));
+		chord.get(1).setSucc(chord.get(2));
+		chord.get(2).setSucc(chord.get(3));
+		chord.get(3).setSucc(chord.get(0));
+
+		chord.get(0).setPred(chord.get(3));
+		chord.get(1).setPred(chord.get(0));
+		chord.get(2).setPred(chord.get(1));
+		chord.get(3).setPred(chord.get(2));
+
+		c1 = new ChordPeer();
+		c2 = new ChordPeer();
+		Random rand = new Random();
+		while (c1.getMyId() == c2.getMyId()) {
+			c2.setMyId(rand.nextInt(101));
+		}
+	}
+
+	public void tearDown() {
+		chord = null;
+		c1 = null;
+		c2 = null;
+	}
+
 	/**
 	 * test la deuxieme condition de findKey
 	 */
-	public void testDeuxiemeConditionFindKey()
-	{
-		ArrayList<ChordPeer> chord = new ArrayList<ChordPeer>();
-		for(int i =0; i<4; i++)
-		{
-			chord.add(new ChordPeer());
-		}
-		chord.get(0).setMyId(8);
-		chord.get(1).setMyId(13);
-		chord.get(2).setMyId(25);
-		chord.get(3).setMyId(96);
-		
-		chord.get(0).setSucc(chord.get(1));
-		chord.get(1).setSucc(chord.get(2));
-		chord.get(2).setSucc(chord.get(3));
-		chord.get(3).setSucc(chord.get(0));
-		
-		chord.get(0).setPred(chord.get(3));
-		chord.get(1).setPred(chord.get(0));
-		chord.get(2).setPred(chord.get(1));
-		chord.get(3).setPred(chord.get(2));
-		
+	public void testDeuxiemeConditionFindKey() {
 		assertEquals(chord.get(0), chord.get(0).findkey(5));
 	}
-	
+
 	/**
-	 * test la troixieme condition de findKey
+	 * test la troisieme condition de findKey
 	 */
-	public void testTroisiemeConditionDeFindKey()
-	{
-		ArrayList<ChordPeer> chord = new ArrayList<ChordPeer>();
-		for(int i =0; i<4; i++)
-		{
-			chord.add(new ChordPeer());
-		}
-		chord.get(0).setMyId(8);
-		chord.get(1).setMyId(13);
-		chord.get(2).setMyId(25);
-		chord.get(3).setMyId(96);
-		
-		chord.get(0).setSucc(chord.get(1));
-		chord.get(1).setSucc(chord.get(2));
-		chord.get(2).setSucc(chord.get(3));
-		chord.get(3).setSucc(chord.get(0));
-		
-		chord.get(0).setPred(chord.get(3));
-		chord.get(1).setPred(chord.get(0));
-		chord.get(2).setPred(chord.get(1));
-		chord.get(3).setPred(chord.get(2));
-		
+	public void testTroisiemeConditionDeFindKey() {
 		assertEquals(chord.get(0), chord.get(3).findkey(2));
 	}
-	
+
 	/**
 	 * test la quatrieme condition de findKey
 	 */
-	public void testQuetriemeConditionDeFindKey()
-	{
-		ArrayList<ChordPeer> chord = new ArrayList<ChordPeer>();
-		for(int i =0; i<4; i++)
-		{
-			chord.add(new ChordPeer());
-		}
-		chord.get(0).setMyId(8);
-		chord.get(1).setMyId(13);
-		chord.get(2).setMyId(25);
-		chord.get(3).setMyId(96);
-		
-		chord.get(0).setSucc(chord.get(1));
-		chord.get(1).setSucc(chord.get(2));
-		chord.get(2).setSucc(chord.get(3));
-		chord.get(3).setSucc(chord.get(0));
-		
-		chord.get(0).setPred(chord.get(3));
-		chord.get(1).setPred(chord.get(0));
-		chord.get(2).setPred(chord.get(1));
-		chord.get(3).setPred(chord.get(2));
-		
+	public void testQuatriemeConditionDeFindKey() {
 		assertEquals(chord.get(2), chord.get(0).findkey(18));
 	}
-	
+
 	/**
 	 * test si les neuds se rajoute bien dans le reseau
 	 */
-	public void testJoinChord()
-	{
-		ChordPeer c1 = new ChordPeer();
-		ChordPeer c2 = new ChordPeer();
-		Random rand = new Random();
-		while(c1.getMyId() == c2.getMyId())
-		{
-			c2.setMyId(rand.nextInt(101));
-		}
+	public void testJoinChord() {
 		c2.joinChord(c1);
-
 		assertEquals(c1, c2.getPred());
 		assertEquals(c1, c2.getSucc());
 
 		assertEquals(c2, c1.getPred());
 		assertEquals(c2, c1.getSucc());
 	}
-	
+
 	/**
 	 * test si les neuds quitte bien le reseau
 	 */
-	public void testLeaveChord()
-	{
+	public void testLeaveChord() {
 		ChordPeer c1 = new ChordPeer();
 		ChordPeer c2 = new ChordPeer();
 		Random rand = new Random();
-		while(c1.getMyId() == c2.getMyId())
-		{
+		while (c1.getMyId() == c2.getMyId()) {
 			c2.setMyId(rand.nextInt(101));
 		}
 		c2.joinChord(c1);
